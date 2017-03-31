f74a7bb360fd04344515526659f144e4c45704c4
Igor Vukelić <igvukelic@gmail.com>
2017-03-31 16:56:14 +0200

    Initial commit. Configure server, flow and babel-node

---
+++ README.md
@@ -0,0 +1,5 @@
+# IN DEVELOPMENT #
+
+# TimerApp #
+
+## React Countdown and Timer App ##
---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+/* @flow */
+
+import Express from 'express'
+import path from 'path'
+import conf from '../conf/'
+
+const APP_PORT: number = conf.APP_PORT
+const app: Express = new Express()
+
+// Middleware
+app.use(Express.static(path.join(__dirname, 'views')))
+
+//Routes
+app.get('*', function(req, res) {
+  res.send('Express route')
+})
+
+app.listen(APP_PORT, function() {
+  console.log(`Express server is up on port ${APP_PORT}`)
+})
