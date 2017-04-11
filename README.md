8196a2c2dd0bbda965d3d43ed4d8b6ab6f57a239
Igor Vukelić <igvukelic@gmail.com>
2017-04-11 21:14:43 +0200

    add tree-shaking support and production webpack.config

--- README.md
+++ README.md
@@ -27,7 +27,8 @@
 
 * `yarn run start-dev` will set NODE_ENV = development and start nodemon. Be sure to make **app.local.json** inside the **conf** folder and designate your *APP_PORT* just like in **app.json**
 * `yarn run clean-client/build-client` will bundle client using webpack
-* `yarn run clean-server/build-server` will bundle server using babel
+* `yarn run clean-server/build-server` will bundle server using babel (development)
+* `yarn run clean-server/build-server-p` will bundle server using babel (production)
 * `yarn run start` will run in production (build the client and server before runing it)
 * `yarn run webpack` will start webpack in watch mode
 * `yarn run flow` will run flow check
---
+++ webpack.config.prod.babel.js
@@ -0,0 +1,96 @@
+'use strict'
+
+import path from 'path'
+import webpack from 'webpack'
+import autoprefixer from 'autoprefixer'
+const publicPath = path.resolve(__dirname, './src/client')
+
+module.exports = {
+  devtool: '#source-maps',
+  performance: {
+    hints: false
+  },
+  context: publicPath,
+  entry: {
+    bundle: [
+      'script-loader!jquery/dist/jquery.min.js',
+      'script-loader!tether/dist/js/tether.min.js',
+      'script-loader!bootstrap/dist/js/bootstrap.min.js',
+      './app.js'
+    ]
+  },
+  output: {
+    path: path.join(publicPath, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      Container: path.resolve(__dirname, 'src/client/scenes/Container.js'),
+      First: path.resolve(__dirname, 'src/client/scenes/first/First.jsx')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules|dist/,
+        loader: 'babel-loader',
+        options: {
+          plugins: [
+            [
+              'babel-plugin-react-css-modules',
+              {
+                context: publicPath,
+                filetypes: {
+                  '.scss': 'postcss-scss'
+                }
+              }
+            ]
+          ],
+          babelrc: false,
+          presets: [
+            'react',
+            ['es2015', { 'modules': false }],
+            'stage-0'
+          ]
+        }
+      },
+      {
+        test: /\.local\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'postcss-loader',
+          'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: path.resolve(__dirname, './src/client/styles/global/sass-resources.scss')
+            }
+          }
+        ]
+      },
+      {
+        test: /^((?!\.local).)+\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      jquery: 'jquery'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      postcss: [autoprefixer]
+    })
+  ]
+}
