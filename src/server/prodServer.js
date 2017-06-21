e0f7db29b8da6c1a97071a91eef5fcd2562094d3
Igor Vukelić <igvukelic@gmail.com>
2017-06-21 23:33:30 +0200

    heroku fix 9

--- src/server/prodServer.js
+++ src/server/prodServer.js
@@ -22,7 +22,7 @@ app.listen(PORT, () => {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-/* set max-age to '1y' (maximum) or 31536000 for client static assets */
+/* set max-age to '1y' (maximum) or 31536000 for client static assets  */
 // app.use(expressStaticGzip(path.join(__dirname, '../', 'dist'), {indexFromEmptyFile: false, maxAge: '1y'}))
 app.use(Express.static(path.join(__dirname, '../', 'dist'), {maxAge: '1y'}))
 
