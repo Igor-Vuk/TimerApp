5befdc84aeb4b6a0a8c7cd5b29f90415b81a569b
Igor Vukelić <igvukelic@gmail.com>
2017-06-21 23:29:22 +0200

    heroku fix 8

--- src/server/prodServer.js
+++ src/server/prodServer.js
@@ -5,7 +5,7 @@ import path from 'path'
 import conf from './conf'
 import appRenderer from './appRenderer'
 import webpackUtils from './webpackUtils'
-var expressStaticGzip = require('express-static-gzip')
+// var expressStaticGzip = require('express-static-gzip')
 
 const APP_PORT: number = conf.APP_PORT
 const PORT: any = process.env.PORT || APP_PORT
@@ -23,8 +23,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 /* set max-age to '1y' (maximum) or 31536000 for client static assets */
-app.use(expressStaticGzip(path.join(__dirname, '../', 'dist'), {indexFromEmptyFile: false, maxAge: '1y'}))
-/*app.use(Express.static(path.join(__dirname, '../', 'dist'), {maxAge: '1y'}))*/
+// app.use(expressStaticGzip(path.join(__dirname, '../', 'dist'), {indexFromEmptyFile: false, maxAge: '1y'}))
+app.use(Express.static(path.join(__dirname, '../', 'dist'), {maxAge: '1y'}))
 
 /* check with the server before using the cached resource */
 app.use((req: Object, res: Object, next: () => void): void => {
