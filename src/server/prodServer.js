ae3273dee2d939b1335a5cde44643cf722d6b551
Igor Vukelić <igvukelic@gmail.com>
2017-06-21 23:05:30 +0200

    heroku fix 7

--- src/server/prodServer.js
+++ src/server/prodServer.js
@@ -5,19 +5,26 @@ import path from 'path'
 import conf from './conf'
 import appRenderer from './appRenderer'
 import webpackUtils from './webpackUtils'
-/*var expressStaticGzip = require('express-static-gzip')*/
+var expressStaticGzip = require('express-static-gzip')
 
 const APP_PORT: number = conf.APP_PORT
 const PORT: any = process.env.PORT || APP_PORT
 
 const app: Express = new Express()
 
+app.listen(PORT, () => {
+  console.log(`
+  Express server is up on port ${PORT}
+  Production environment
+  `)
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 /* set max-age to '1y' (maximum) or 31536000 for client static assets */
-/*app.use(expressStaticGzip(path.join(__dirname, '../', 'dist'), {indexFromEmptyFile: false, maxAge: '1y'}))*/
-app.use(Express.static(path.join(__dirname, '../', 'dist'), {maxAge: '1y'}))
+app.use(expressStaticGzip(path.join(__dirname, '../', 'dist'), {indexFromEmptyFile: false, maxAge: '1y'}))
+/*app.use(Express.static(path.join(__dirname, '../', 'dist'), {maxAge: '1y'}))*/
 
 /* check with the server before using the cached resource */
 app.use((req: Object, res: Object, next: () => void): void => {
@@ -26,7 +33,7 @@ app.use((req: Object, res: Object, next: () => void): void => {
 })
 
 /* Use server side rendering for first load */
-// app.use(appRenderer)
+app.use(appRenderer)
 
 /* Use CommonChunks and long term caching */
 app.use(webpackUtils)
@@ -37,9 +44,4 @@ app.get('*', (req: Object, res: Object) => {
   res.render('index', {app: req.body, webpack: req.chunk})
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`
-  Express server is up on port ${PORT}
-  Production environment
-  `)
-})
+
