37d002a161d95364a135241ab084388d2f75433a
Igor Vukelić <igvukelic@gmail.com>
2017-07-09 04:31:07 +0200

    ggh

--- src/client/scenes/shared/navigation/index.jsx
+++ src/client/scenes/shared/navigation/index.jsx
@@ -19,7 +19,7 @@ class Navigation extends Component {
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
-          {/* navigation bar brend */}
+          {/* navigation bar brend  */}
           <a className="navbar-brand" href="#">ReactTimer</a>
 
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
